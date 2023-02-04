296d773e2a8e935f410ea88d23775c0bf9172600
NisansalaSenevirathna <92750030+NisansalaSenevirathna@users.noreply.github.com>
2023-02-04 20:35:06 +0530

    add css admin-view-cho

--- views/admin/communityheadoffices/view.php
+++ views/admin/communityheadoffices/view.php
@@ -2,31 +2,85 @@
 use app\core\components\tables\table;
 /** @var $model \app\models\choModel */
 
-$communitycheadofice = $model->retrieve();
 
-$header = ["District", "Email", "Address", "ContactNumber"];
 
-$arraykey = ["district", "email", "address", "contactNumber"];
 
-$communitycheadoficeTable=new table($header,$arraykey);
-
-$communitycheadoficeTable->displayTable($communitycheadofice)
-
-//echo "<pre>";
-//print_r($chos);
-//echo "</pre>";
 
 ?>
 
 
-<?php $regForm = \app\core\components\form\form::begin('logout', 'post'); ?>
+        <!--        Profile Details-->
+        <div class="profile">
+            <div class="notif-box">
+                <i class="material-icons">notifications</i>
+            </div>
+            <div class="profile-box">
+                <div class="name-box">
+                    <h4>Username</h4>
+                    <p>Position</p>
+                </div>
+                <div class="profile-img">
+                    <img src="https://www.w3schools.com/howto/img_avatar.png" alt="profile">
+                </div>
+            </div>
+        </div>
 
-<button> logout </button>
+        <!--   Heading Block - Other Pages for Ongoing, Completed .etc      -->
+        <div class="heading-pages">
+            <div class="heading">
+                <h1>Heading</h1>
+            </div>
+            <div class="pages">
+                <a href="#">
+                    <i class="material-icons">cached</i>
+                    Ongoing</a>
+                <a href="#">
+                    <i class="material-icons">check_circle_outline</i>
+                    Completed</a>
+                <a href="#">
+                    <i class="material-icons">block</i>
+                    Cancelled</a>
+            </div>
+            <?php $registerCho = \app\core\components\form\form::begin('./communityheadoffices/register','post') ?>
 
-<?php $regForm->end(); ?>
+    <button> Register a Cho</button>
 
-<?php $registerCho = \app\core\components\form\form::begin('./communityheadoffices/register','post') ?>
+    <?php $registerCho->end(); ?>
 
-<button> Register a Cho</button>
+        </div>
 
-<?php $registerCho->end(); ?>
+        <!--        Search and filter boxes -->
+        <div class="search-filter">
+
+            <div class="filters">
+                <div class="filter">
+                    <p><i class="material-icons">filter_list</i><span>Filter</span></p>
+                </div>
+                <div class="sort">
+                    <p><i class="material-icons">sort</i> <span>Sort</span></p>
+                </div>
+            </div>
+            <div class="search">
+                <input type="text" placeholder="Search">
+                <a href="#"><i class="material-icons">search</i></a>
+            </div>
+
+        </div>
+
+        <!--        Content Block-->
+        <div class="content">
+            <div class="filler">
+        <?php    $communitycheadofice = $model->retrieve();
+
+       $header = ["District", "Email", "Address", "ContactNumber"];
+
+       $arraykey = ["district", "email", "address", "contactNumber"];
+
+       $communitycheadoficeTable=new table($header,$arraykey);
+
+       $communitycheadoficeTable->displayTable($communitycheadofice)
+?>
+            </div>
+        </div>
+
+  
