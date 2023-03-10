949d05716c39ce940a184223e5b1c6b87b84c140
Gineth Karunanayake <gavishkak@gmail.com>
2023-03-10 11:31:33 +0530

    Completed the CC registration

--- views/cho/CC/register.php
+++ views/cho/CC/register.php
@@ -1,12 +1,12 @@
+<link rel="stylesheet" href="../../public/CSS/button/button-styles.css">
 <?php
-
 /** @var $cc \app\models\ccModel */
 /** @var $user \app\models\userModel */
 
 ?>
 
 <?php
-echo $_SESSION=['user'];
+//echo $_SESSION=['user'];
 echo "<pre>";
 if(empty(\app\core\Application::session()->getFlash('success'))){
     print_r(\app\core\Application::session()->getFlash('error'));
@@ -16,24 +16,59 @@ if(empty(\app\core\Application::session()->getFlash('success'))){
 }
 echo '</pre>';
 ?>
+<!--        Profile Details-->
+<div class="profile">
+    <div class="notif-box">
+        <i class="material-icons">notifications</i>
+    </div>
+    <div class="profile-box">
+        <div class="name-box">
+            <h4>Username</h4>
+            <p>Position</p>
+        </div>
+        <div class="profile-img">
+            <img src="https://www.w3schools.com/howto/img_avatar.png" alt="profile">
+        </div>
+    </div>
+</div>
+
+<!--   Heading Block - Other Pages for Ongoing, Completed .etc      -->
+<div class="heading-pages">
+    <div class="heading">
+        <h1>Create a Community Head Office</h1>
+    </div>
+</div>
+
+<div class="content">
 
 <?php $ccRegistrationForm = \app\core\components\form\form::begin('','post') ?>
+<div class="login-grid-2">
+    <div class="form-split">
+        <?php $ccRegistrationForm->inputField($cc,"Address",'text','address') ?>
 
-<?php $ccRegistrationForm->inputField($cc,"Address",'text','address') ?>
+        <?php $ccRegistrationForm->inputField($cc,"City",'text','city') ?>
 
-<?php $ccRegistrationForm->inputField($cc,"City",'text','city') ?>
+        <?php $ccRegistrationForm->inputField($cc,"Email",'email','email') ?>
 
-<?php $ccRegistrationForm->inputField($cc,"Email",'email','email') ?>
+        <?php $ccRegistrationForm->inputField($cc,"Fax",'text','fax')?>
+
+        <?php $ccRegistrationForm->inputField($cc,"ContactNumber",'text','contactNumber')?>
+
+    </div>
+    <div class="form-split">
 
 
-<?php $ccRegistrationForm->inputField($cc,"Fax",'text','fax')?>
+        <?php $ccRegistrationForm->inputField($user, "Username",'text','username') ?>
 
-<?php $ccRegistrationForm->inputField($cc,"ContactNumber",'text','contactNumber')?>
+        <?php $ccRegistrationForm->inputField($user, "Password",'password','password') ?>
 
-<?php $ccRegistrationForm->inputField($user, "username",'text','username') ?>
+    </div>
+    <br>
+    <div >
+        <?php $ccRegistrationForm->button("Confirm") ?>
+    </div>
 
-<?php $ccRegistrationForm->inputField($user, "Password",'password','password') ?>
-
-<?php $ccRegistrationForm->button("Confirm") ?>
+</div>
 
 <?php $ccRegistrationForm->end() ?>
+</div>
