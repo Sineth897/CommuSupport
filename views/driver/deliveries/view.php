94f531810476fa833cf6afa250a5684471fe0870
THAMUDITHA P V GS <2020cs183@stu.ucsc.cmb.ac.lk>
2023-04-17 07:42:40 +0530

    Driver delivery card added

--- views/driver/deliveries/view.php
+++ views/driver/deliveries/view.php
@@ -1,7 +1,10 @@
+<link rel="stylesheet" href="../public/CSS/cards/driver-delivery-card.css">
 <?php
 
 /** @var $deliveries \app\models\deliveryModel */
 /** @var $user \app\models\driverModel */
+
+$AssignedDeliveries = $deliveries->getAssignedDeliveries($_SESSION['user']);
 
 ?>
 
@@ -41,4 +44,31 @@ $searchDiv->end(); ?>
 
 <div class="card-container">
 
+    <?php
+
+//    echo "<pre>";
+//    print_r($AssignedDeliveries);
+//    echo "</pre>";
+
+    $deliveryCard = new \app\core\components\cards\driverDeliveryCard();
+
+    $deliveryCard->showAssignedDeliveries($AssignedDeliveries);
+
+
+
+    ?>
+
+<!--    <div class="driver-del-card">-->
+<!--        <div class="card-column subcategory"><strong>Sub Category</strong><p>Gas Cylinder</p></div>-->
+<!--        <div class="card-column pickupaddress"><strong>Pick up</strong><p>123/5, Colombo, Colombo</p></div>-->
+<!--        <div class="card-column deliveryaddress"><strong>Drop Off</strong><p>123/5, Colombo, Colombo</p></div>-->
+<!--        <div class="card-column assigneddate"><strong>Created Date</strong><p>2023-01-45</p></div>-->
+<!--        <div class="card-column route-complete-btns">-->
+<!--            <a class="del-route" href="#">Route</a>-->
+<!--            <a class="del-accept" href="#">Accept</a>-->
+<!--        </div>-->
+<!--        <div class="card-column delivery-btns">-->
+<!--            <a class="del-decline" href="#">Request to Re-Assign</a>-->
+<!--        </div>-->
+<!--    </div>-->
 </div>
