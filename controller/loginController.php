4d86aa6622a0bf9943564b6bbbddff83e7572b08
THAMUDITHA P V GS <2020cs183@stu.ucsc.cmb.ac.lk>
2023-05-09 23:32:38 +0530

    Commented some lines

--- controller/loginController.php
+++ controller/loginController.php
@@ -4,6 +4,8 @@ namespace app\controller;
 
 use app\core\Application;
 use app\core\Controller;
+use app\core\exceptions\forbiddenException;
+use app\core\exceptions\methodNotFound;
 use app\core\middlewares\loginMiddleware;
 use app\core\Request;
 use app\core\Response;
@@ -16,105 +18,204 @@ class loginController extends  Controller
 {
 
 
-
-    public function __construct(string $func,Request $request,Response $response)
+    /**
+     * @param string $func
+     * @param Request $request
+     * @param Response $response
+     * @throws forbiddenException
+     * @throws methodNotFound
+     */
+    public function __construct(string $func, Request $request, Response $response)
     {
         $this->middleware = new loginMiddleware();
         parent::__construct($func, $request, $response);
 
     }
 
-    protected function userLogin($request, $response)
+    /**
+     * @param $request
+     * @param $response
+     * @return void
+     */
+    protected function userLogin($request, $response) : void
     {
 
+        // if user is already logged in, redirect to home page
         $this->ifLoggedIn($response);
 
+
         $user = new userModel();
+
+        // if request is a post request, get the data from the request body and try to log in
         if ($request->isPost()) {
+
+            // load data from the login form
             $user->getData($request->getBody());
+
+            // if login is successful, redirect to home page
             if ($user->login()) {
+
+                // if remember me is clicked, set the remember me cookie
                 if($this->isRememberMeClicked($request)) {
+
+                    // set the remember me cookie
                     $this->rememberMe($user);
                 }
+
+                // redirect to home page
                 $response->redirect('/');
                 return;
             }
         }
 
+        // render the login page
         $this->render("login/user", "User Login", [
             'user' => $user
         ]);
     }
 
-    protected function employeeLogin(Request $request, Response $response)
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @return void
+     */
+    protected function employeeLogin(Request $request, Response $response) :void
     {
 
+        // if user is already logged in, redirect to home page
         $this->ifLoggedIn($response);
 
         $user = new userModel();
+
+        // if request is a post request, get the data from the request body and try to log in
         if ($request->isPost()) {
+
+            // load data from the login form
             $user->getData($request->getBody());
+
+            // if login is successful, redirect to home page
             if ($user->login(true)) {
+
+                // if remember me is clicked, set the remember me cookie
                 if($this->isRememberMeClicked($request)) {
+
+                    // set the remember me cookie
                      $this->rememberMe($user);
                 }
+
+                // redirect to home page
                 $response->redirect('/');
                 return;
+
             }
         }
 
+        // render the login page
         $this->render("login/employee", "Employee Login",[
             'user' => $user
         ]);
+
     }
 
-    protected function logout(Request $request, Response $response)
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @return void
+     */
+    protected function logout(Request $request, Response $response) : void
     {
+
+        // remove the remember me cookie
         $this->forgetMe();
+
         $user = new userModel();
+
+        // logout the user
         $user->logout();
+
+        // set the flash message and redirect to home page
         $this->setFlash('success','Logout Successful');
         $response->redirect('/');
+
     }
 
-    protected function forgetPassword(Request $request,Response $response) {
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @return void
+     */
+    protected function forgetPassword(Request $request, Response $response) : void {
         try{
+
+            // if the request is a post request, get the data from the request body and send the OTP
             if($request->isPost()) {
+
+                // get the data from the request JSON body
                 $data = $request->getJsonData();
+
+                // get the function name from the request body unset it from the data
                 $func = $data['do'];
                 unset($data['do']);
+
+                // call the function
                 $result = $this->$func($data);
+
+                // send the result as JSON
                 $this->sendJson($result);
             }
         }
         catch(\Exception $e) {
+
+            // if any exception occurs, send the exception message as JSON
             $this->sendJson(['success' => 0, 'message' => $e->getMessage()]);
         }
 
+        // if the request is a get request, render the forget password page
         if($request->isGet()) {
             $this->render("login/forgetPassword/forgetPassword", "Forget Password");
         }
 
     }
 
-    protected function lockedAccount(userModel $model)
+    protected function lockedAccount(userModel $model) : void
     {
 //        $username = $model->username;
         echo " is locked";
     }
 
-    private function changePassword($data):array {
+    /**
+     * @param $data
+     * @return array
+     */
+    private function changePassword($data) : array {
+
+        // get the user model from the database
         $user = userModel::getModel(['username' => $data['username']]);
+
+        // change password using the model
         if($user->changePassword($data['newPassword'])) {
             return ['success' => 1, 'message' => 'Password changed successfully'];
         }
+
+        // if password change fails, return error message
         return ['success' => 0, 'message' => 'Password change failed'];
+
     }
 
-    private function requestOTP($data):array {
+    /**
+     * @param $data
+     * @return array
+     */
+    private function requestOTP($data) : array {
         try {
+
+            // generate random number for OTP
             $OTP = rand(100000,999999);
+
+            // created time
             $createdTime = time();
+
+            // valid for 10 mins
             $validTime = $createdTime + 60 * 10;
         }
         catch (\Exception $e) {
@@ -126,55 +227,120 @@ class loginController extends  Controller
             'createdTime' => $createdTime,
             'validTime' => $validTime
         ];
+
+        // set the OTP in session
         $this->setSessionMsg('OTP', $OTP);
+
+        // get the user model from the database
         $user = userModel::getModel(['username' => $data['username']]);
+
+        // send the OTP to the user
         if($this->sendOTP($OTP['OTP'], $user)) {
             return ['success' => 1, 'message' => 'OTP sent'];
         }
+
+        // if OTP sending fails, return error message
         return ['success' => 0, 'message' => 'Unable to send OTP'];
+
     }
 
-    private function checkOTP($data):array {
+    /**
+     * @param $data
+     * @return array
+     */
+    private function checkOTP($data) : array {
+
+        // get the OTP from session
         $OTP = $this->getSessionMsg('OTP');
+
+        // if OTP is not found, return error message
         if(!$OTP) {
             return ['success' => 0, 'message' => 'OTP not found. '];
         }
+
+        // if OTP is expired, return error message
         if($OTP['validTime'] < time()) {
             return ['success' => 0, 'message' => 'OTP is expired. Please request a new OTP'];
         }
+
+        // if OTP does not match, return error message
         if($OTP['OTP'] != $data['OTP']) {
             return ['success' => 0, 'message' => 'OTP does not match'];
         }
+
+
         $user = userModel::getModel(['username' => $data['username']]);
+
+        // if OTP is valid, return success message
+        // if user is employee, return isEmployee = true
         $isEmployee = $user->isEmployee($user->userType);
+
+        // unset the OTP from session
         $this->unsetCookie('OTP');
+
+        // return success message
         return ['success' => 1, 'message' => 'OTP is valid', 'isEmployee' => $isEmployee];
+
     }
 
-    private function checkUsername($data):array {
+    /**
+     * @param $data
+     * @return array
+     */
+    private function checkUsername($data) : array {
         $username = $data['username'];
         $user = userModel::getModel(['username' => $username]);
         if(!$user) {
             return ['success' => 0,'message' => 'User account with given username does not exist'];
         }
         return ['success' => 1, 'message' => 'User account found'];
+
     }
 
-    private function ifLoggedIn(Response $response) {
 
+    /**
+     * @param Response $response
+     * @return void
+     */
+    private function ifLoggedIn(Response $response) : void {
+
+        // if already logged in redirect to user's home page
         if($this->getUserType() !== 'guest') {
+
+            // set the flash message and redirect to home page
             $this->setFlash('loggedStatus', 'You are already logged in');
             $response->redirect('/');
+
         }
+
     }
 
-    private function isRememberMeClicked(Request $request):bool {
+    /**
+     * @param Request $request
+     * @return bool
+     */
+    private function isRememberMeClicked(Request $request) : bool {
+
+        // if remember me is clicked, return true
         return !empty($request->getBody()['rememberMe']);
+
     }
 
-    private function rememberMe(userModel $user, $days = 30):void {
+    /**
+     * @param userModel $user
+     * @param int $days
+     * @return void
+     */
+    private function rememberMe(userModel $user, $days = 30) : void {
+
+        // get the user model from the database
         $user = $user->findOne(['username' => $user->username]);
+
+        // generate selector, validator and token
         [$selector, $validator, $token] = ['', '', ''];
+
+        // generate selector, validator and token until all of them are generated
+        // this is to make sure that all of them are generated, because generating built-in function is not 100% secure
         while(!($selector && $validator && $token)) {
             [$selector, $validator, $token] = $this->generateSelectorNValidator();
         }
